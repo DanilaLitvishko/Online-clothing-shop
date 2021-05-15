f35de27d9a6a7b5b4c355309b4035927bee41ea6
DanilaLitvishko <litvishko30@mail.ru>
2021-05-15 12:31:39 +0300

    added shop page with collection preview and collection items

--- src/App.js
+++ src/App.js
@@ -2,50 +2,17 @@ import React from 'react';
 import './App.css';
 import HomePage from './pages/homepage/homepage.component'
 import {Switch, Route} from 'react-router-dom';
+import ShopPage from './pages/shop/shop.component'
 
 function App() {
   return (
     <div> 
       <Switch>
         <Route exact path='/' component={HomePage}/>
-        <Route path='/hats' component={HatsPage}/>
-        <Route path='/jackets' component={JacketsPage}/>
-        <Route path='/sneakers' component={SneakersPage}/>
-        <Route path='/womens' component={WomensPage}/>
-        <Route path='/mens' component={MensPage}/>
+        <Route path='/shop' component={ShopPage}/>
       </Switch>
     </div>
   );
 }
-
-const HatsPage = () => (
-  <div>
-    <h1>HATS PAGE</h1>
-  </div>
-)
-
-const JacketsPage = () => (
-  <div>
-    <h1>JACKETS PAGE</h1>
-  </div>
-)
-
-const SneakersPage = () => (
-  <div>
-    <h1>SNEAKERS PAGE</h1>
-  </div>
-)
-
-const WomensPage = () => (
-  <div>
-    <h1>WOMENS NOT HUMAN PAGE</h1>
-  </div>
-)
-
-const MensPage = () => (
-  <div>
-    <h1>MENS PAGE</h1>
-  </div>
-)
 
 export default App;
