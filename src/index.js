f3644104f5702362722c9cc62c7fef49ef129314
Danila Litvishka <litvishko30@mail.ru>
2021-05-19 11:34:17 +0300

    added functinality with localStorage, info in checkout-page don't disappear

--- src/index.js
+++ src/index.js
@@ -2,25 +2,19 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-<<<<<<< HEAD
-import * as serviceWorkerRegistration from './serviceWorkerRegistration';
-import reportWebVitals from './reportWebVitals';
-import {BrowserRouter} from 'react-router-dom';
-
-ReactDOM.render(
-  <BrowserRouter><App /></BrowserRouter>,
-=======
 import {BrowserRouter} from 'react-router-dom';
 import {Provider} from 'react-redux';
+import {PersistGate} from 'redux-persist/integration/react'
 
-import store from './redux/store'
+import {store, persistor} from './redux/store'
 
 ReactDOM.render(
   <Provider store={store}>
     <BrowserRouter>
-      <App />
+      <PersistGate persistor={persistor}>
+        <App />
+      </PersistGate>
     </BrowserRouter>
   </Provider>,
->>>>>>> added card icon and cart dropdown
   document.getElementById('root')
 );
