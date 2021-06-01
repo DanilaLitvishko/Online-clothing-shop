ab94925e744bea1efcac2e745581a53be00f7c48
Danila Litvishka <litvishko30@mail.ru>
2021-06-01 12:44:38 +0300

    optimization frontend with react-lazy and react-Suspence

--- client/src/App.js
+++ client/src/App.js
@@ -6,15 +6,16 @@ import { createStructuredSelector } from 'reselect'
 import { GlobalStyle } from './global.styles'
 
 import Header from './components/header/header.component'
-import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component'
-import CheckoutPage from './pages/checkout/checkout.component'
-import ShopPage from './pages/shop/shop.component'
-import NotFound from './pages/not-found/not-found.component'
+import Spinner from './components/spinner/spinner.component'
+import ErrorBoundary from './components/error-boundary/error-boundary.component'
 
 import { selectCurrentUser } from './redux/user/user.selectors'
 import {checkUserSession} from './redux/user/user.actions'
 
 const HomePage = lazy(() => import('./pages/homepage/homepage.component'))
+const ShopPage = lazy(() => import ('./pages/shop/shop.component'))
+const SignInAndSignUpPage = lazy(() => import ('./pages/sign-in-and-sign-up/sign-in-and-sign-up.component'))
+const CheckoutPage = lazy(() => import ('./pages/checkout/checkout.component'))
 
 const App = ({checkUserSession, currentUser}) => {
   
@@ -27,13 +28,14 @@ const App = ({checkUserSession, currentUser}) => {
       <GlobalStyle/> 
         <Header />
         <Switch>
-          <Suspense fallback={<div>...</div>}>
-            <Route exact path='/' component={HomePage}/>
-          </Suspense>
-          <Route path='/shop' component={ShopPage}/>
-          <Route exact path='/checkout' component={CheckoutPage}/>
-          <Route exact path='/signin' render={() => currentUser? (<Redirect to='/'/>) : (<SignInAndSignUpPage/>)}/>
-          <Route component={NotFound}/>
+          <ErrorBoundary>
+            <Suspense fallback={<Spinner/>}>
+              <Route exact path='/' component={HomePage}/>
+              <Route path='/shop' component={ShopPage}/>
+              <Route exact path='/checkout' component={CheckoutPage}/>
+              <Route exact path='/signin' render={() => currentUser? (<Redirect to='/'/>) : (<SignInAndSignUpPage/>)}/>
+            </Suspense>
+          </ErrorBoundary>
         </Switch>
     </div>
   );
--- client/src/pages/shop/shop.component.jsx
+++ client/src/pages/shop/shop.component.jsx
@@ -1,12 +1,14 @@
-import React, {useEffect} from 'react'
+import React, {useEffect, lazy, Suspense} from 'react'
 import {Route} from 'react-router-dom'
 
 import {connect} from 'react-redux'
 
 import { fetchCollectionsStart} from '../../redux/shop/shop.actions'
 
-import CollectionsOverviewContainer from '../../components/collections-overview/collection-overview.container'
-import CollectionPageContainer from '../collection/collection.container'
+import Spinner from '../../components/spinner/spinner.component'
+
+const CollectionPageContainer = lazy(() => import('../collection/collection.container'))
+const CollectionsOverviewContainer = lazy(() => import('../../components/collections-overview/collection-overview.container'))
 
 const ShopPage = ({ fetchCollectionsStart, match }) => {
 
@@ -16,14 +18,16 @@ const ShopPage = ({ fetchCollectionsStart, match }) => {
 
     return (
         <div className="shop-page">
-            <Route 
-                exact path={`${match.path}`}  
-                component={CollectionsOverviewContainer}
-            />
-            <Route 
-                path={`${match.path}/:collectionId`} 
-                component={CollectionPageContainer}
-            />
+            <Suspense fallback={<Spinner/>}>
+                <Route 
+                    exact path={`${match.path}`}  
+                    component={CollectionsOverviewContainer}
+                />
+                <Route 
+                    path={`${match.path}/:collectionId`} 
+                    component={CollectionPageContainer}
+                />
+            </Suspense>
         </div>
     )
 } 
