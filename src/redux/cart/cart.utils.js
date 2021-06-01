96fd5eb318d0c91f66c36f52bc946a29f07ad715
Danila Litvishka <litvishko30@mail.ru>
2021-06-01 13:36:50 +0300

    refactor existingCartItem

--- src/redux/cart/cart.utils.js
+++ src/redux/cart/cart.utils.js
@@ -1,11 +1,8 @@
-function checkExisting(arr, val) {
-    return arr.some(function(arrVal) {
-      return val.id === arrVal.id;
-    });
-  }
-
 export const addItemToCart = (cartItems, cartItemToAdd) =>{
-    const existingCartItem = checkExisting(cartItems, cartItemToAdd)
+    const existingCartItem = (cartItems.some((cartItem) => {
+        cartItem.id === cartItemToAdd.id;
+      }))
+
     if(existingCartItem){
         return cartItems.map(cartItem=>
             cartItem.id === cartItemToAdd.id
